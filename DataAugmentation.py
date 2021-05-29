6ce2afd9b57aa5d7a95719c2d0bb3058a2053e98
Eric Chen <48618187+EricChenYJ@users.noreply.github.com>
2021-05-29 22:40:18 +0800

    Create DataAugmentation.py

---
+++ DataAugmentation.py
@@ -0,0 +1,25 @@
+import os
+import cv2
+
+input_dir=''
+augmented_dir=''
+
+
+
+
+def horizontal_flip():
+    pass
+
+def vertical_flip():
+    pass
+
+def random_brightness():
+    pass
+
+
+
+
+if __name__=="__main__":
+    horizontal_flip()
+    vertical_flip()
+    random_brightness()
